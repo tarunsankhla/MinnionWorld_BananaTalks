bc520c0f2ec3c658f0091da572c32e6efa6f2932
Tether <tarun.sankhla.prc@gmail.com>
2021-09-14 10:13:15 +0530

    input output processing done

--- app.js
+++ app.js
@@ -2,10 +2,23 @@
 
 // var username = prompt("ou name");
 // alert("ok"+username);
+
+// linkedin requesr acceptor
+// document.querySelectorAll("").forEach(item => setTimeout(item.click(), Math.random +1000));
+
+
+// document.querySelector("testares");
+// document.querySelector("#translate_button");
+// document.querySelector(".translate_button");
+// document.querySelector("input[name='translate_button']");
+
+// document.getElementById("Input_item").value;
+
 var btnTranslate  = document.querySelector("#translate_button");
 var input  = document.querySelector("#Input_item");
-
+var outputdiv  = document.querySelector("#output");
+//  outputdiv.innerText = "hell";
 
 btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("Clicked" +input.value);
+    outputdiv.innerHTML = "Translated" + input.value    ;
 })
