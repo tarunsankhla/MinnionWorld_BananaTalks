22aace0e51e85c607967bba357f93b87936f9b7f
Tether <tarun.sankhla.prc@gmail.com>
2021-09-15 18:42:26 +0530

    Added fetch and promise

--- app.js
+++ app.js
@@ -14,6 +14,16 @@
 
 // document.getElementById("Input_item").value;
 
+// fetch(url)
+// .then(res => res.json())
+// .then(json =>  console.log(json))
+
+// fetch(url)
+// .then(function responseHandler(response){return response.json()})
+// .then(function LogJSON(json){ console.log(json)})
+
+// var url_ ="https://lesson4apitarun-1.tarunsankhla.repl.co/translate/yoda.json?text= I am tarun"
+
 var btnTranslate  = document.querySelector("#translate_button");
 var input  = document.querySelector("#Input_item");
 var outputdiv  = document.querySelector("#output");
